8aee35514acd5ce1a61d4089c71b9a26e0b5de89
Ein_Green <mbananola2002@gmail.com>
2021-03-29 21:45:33 -0700

    created multiple variables, make enemy cookies, and a funciton for the condition of the game

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@
 function selectPokemon(selectedItem) {
     Cookies.set("selectedPokemon", selectedItem);
 }
+function pokemonSetUp(selectedPokemon, num) {
+    Cookies.set("selectedPokemon", selectedPokemon);
+    Cookies.set("pokemonDefaultHp", num);
+    Cookies.set("pokemonCurrentHp", num);
+}
+
+function enemySetUp(enemyPokemon, number) {
+    Cookies.set("enemyPokemon", enemyPokemon);
+    Cookies.set("enemyDefaultHp", number);
+    Cookies.set("enemyCurrentHp", number);
+}
 
 // these are variables for the pokemon cards
 var selectedSqurtle = document.getElementById(`squirtleCard`);
@@ -9,8 +20,7 @@ var selectedBulbasaur = document.getElementById(`bulbasaurCard`);
 var selectedCloud = document.getElementById(`fFCard`);
 
 // this block of code give the cards thier cookies when user clicks on said card
-selectedSqurtle.onclick = function() {selectPokemon(`squrtle`)};
-selectedBulbasaur.onclick = function() {selectPokemon(`bulbasaur`)};
-selectedCloud.onclick = function() {selectPokemon(`cloud`)};
+selectedSqurtle.onclick = function() {pokemonSetUp(`squrtle`, 110); enemySetUp(`charmander`, 125);};
+selectedBulbasaur.onclick = function() {pokemonSetUp(`bulbasaur`, 100); enemySetUp(`charmander`, 125);};
+selectedCloud.onclick = function() {pokemonSetUp(`cloud`, 150); enemySetUp(`charmander`, 125);};
 // note: glad I made this instead of cheating with HTML
-
--- js/battle.js
+++ js/battle.js
@@ -1,40 +1,64 @@
+// variables for Player:
 var selectedFighter = Cookies.get(`selectedPokemon`);
+var pokemonDefaultHp = Cookies.get(`pokemonDefaultHp`);
+var pokemonCurrentHp = Cookies.get(`pokemonCurrentHp`);
+// variables for Hp:
+var enemyFighter = Cookies.get(`enemyPokemon`);
+var enemyDefaultHp = Cookies.get(`enemyDefaultHp`);
+var enemyCurrentHp = Cookies.get(`enemyCurrentHp`);
+// variables for HTML File:
 var battleSimulator = document.getElementById(`battleSimulator`);
-// variables for Fighters
+var winMessage = document.getElementsByClassName(`winMessage`);
+// variables for Fighters:
 var usersFighter = document.getElementById(`usersFighter`);
 var simulateSquirtle = document.getElementById(`simulateSquirtle`);
 var simulateBulbasaur = document.getElementById(`simulateBulbasaur`);
 var simulateFinal = document.getElementById(`simulateFinal`);
-// variables for HP
+// variables for HP control:
 var playerhealth = document.getElementById("playerhealth");
 var enemyhealth = document.getElementById("enemyhealth"); 
-
+// move variables:
 var bulbasaurAttack = document.getElementById(`bulbasaurAttack`);
 var bulbasaurHeal = document.getElementById(`bulbasaurHeal`);
 
+
+
+playerhealth.value = pokemonDefaultHp;
+enemyhealth.value = enemyDefaultHp
+
 function deal(num) {
     var dealt = enemyhealth.value -= num;
+    Cookies.set(`enemyCurrentHp`, dealt);
     return dealt;
 }
 
-function take(num) {
-    var taken = playerhealth.value -= num;
+function take(number) {
+    var taken = playerhealth.value -= number;
+    Cookies.set(`pokemonCurrentHp`, taken);
     return taken;
 }
 
-bulbasaurAttack.onclick = function() {deal(10)};
-
-// function heal(num) {
-//     var heal = playerhealth.value += num;
-//     return heal;
-// }
-
-// bulbasaurHeal.onclick = function() {heal(15)};
-
+function gameCondition() {
+    if (enemyhealth.value === 0) {
+        winMessage.innerText = `${selectedFighter} Wins!`
+    } else if (playerhealth.value === 0) {
+        winMessage.innerText = `Game Over. Better Luck Next Time`
+    }
+}
 
 if (selectedFighter === undefined) {
     console.log(`user has not selected a fighter`);
 } else {
     usersFighter.innerText = `You have chosen: ${selectedFighter}`;
 }
+
+bulbasaurAttack.onclick = function() {
+    deal(10); 
+    take(5);
+};
+
+
+
+
+
 
